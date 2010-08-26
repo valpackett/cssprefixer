c0c84491d586607370928cf651191b8e8aa016df
myfreeweb <me@myfreeweb.ru>
2010-08-26 17:19:29 +0400

    More tests

--- tests.py
+++ tests.py
@@ -11,5 +11,13 @@ class PrefixerTestCase(unittest.TestCase):
         self.assertEqual(cssprefixer.process('a{transform: rotate(10deg)}', minify=True),
                          'a{transform:rotate(10deg);-webkit-transform:rotate(10deg);-moz-transform:rotate(10deg);-o-transform:rotate(10deg)}')
 
+    def test_webkit(self):
+        self.assertEqual(cssprefixer.process('a{background-clip: padding-box}', minify=True),
+                         'a{background-clip:padding-box;-webkit-background-clip:padding-box}')
+
+    def test_ie_and_opera(self):
+        self.assertEqual(cssprefixer.process('a{text-overflow: ellipsis}', minify=True),
+                         'a{text-overflow:ellipsis;-o-text-overflow:ellipsis;-ms-text-overflow:ellipsis}')
+
 if __name__ == '__main__':
     unittest.main()
